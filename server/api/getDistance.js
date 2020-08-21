ecee12f1ad64c7625a72832ea5144f636fe0551c
Alexander A Lizarraga <alexander_lizarraga@ymail.com>
2020-08-20 18:41:55 -0700

    initial commit adding main functionality to zipcode distance app

---
+++ server/api/getDistance.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+const bodyParser = require('body-parser');
+router.use(bodyParser.urlencoded({ extended: true }));
+router.use(bodyParser.json());
+
+//importing zipCode data file
+let zipCodes = require('../allZipCodes').zipCodes;
+zipCodes = JSON.parse(zipCodes);
+
+//importing calculate distance function
+const calculateDistance = require('../helper/calculateDistance').calculateDistance;
+
+//returns all equipments in the db
+router.get('/', (req, res) => {
+    console.log('zipcode test')
+    console.log(req.query)
+    if (req.query && req.query.zipcode1 && req.query.zipcode2) {
+        res.send({
+            message: 'completed your request',
+            zipcode1: zipCodes[req.query.zipcode1],
+            zipcode2: zipCodes[req.query.zipcode2],
+            distance: calculateDistance(zipCodes[req.query.zipcode1], zipCodes[req.query.zipcode2])
+        })
+    } else {
+        res.send('not valid query')
+    }
+
+});
+
+module.exports = router;
---
+++ server/app.js
@@ -0,0 +1,124 @@
+//express server import
+const express = require('express');
+
+// CORS
+//const cors = require('cors');
+
+//body parser
+const bodyParser = require('body-parser');
+
+//file path related imports
+const path = require('path');
+const fs = require('fs');
+const util = require('util');
+
+//initialize express server app
+const app = express();
+
+//set port for server
+app.set('port', (process.env.Port || 3002));
+
+//use CORS
+//app.use(cors())
+
+//read file
+const readline = require('readline');
+
+
+//Routes
+const distance = require('./api/getDistance');
+
+app.use('/api/getDistance', distance);
+
+app.listen(app.get('port'), () => {
+    console.log(`Server is running on port ${app.get('port')}`);
+})
+
+//const zipCodes = {}
+
+// //census file to js object to file
+// async function processLineByLine() {
+//     const fileStream = fs.createReadStream('./zipData/2019_Gaz_zcta_national.txt');
+
+//     const rl = readline.createInterface({
+//         input: fileStream,
+//         crlfDelay: Infinity
+//     });
+//     // Note: we use the crlfDelay option to recognize all instances of CR LF
+//     // ('\r\n') in input.txt as a single line break.
+
+//     for await (const line of rl) {
+//         // Each line in input.txt will be successively available here as `line`.
+//         console.log(`Line from file: ${line}`);
+//         str = line.trim().split('\t')
+//         //str = str.replace(/  +/g, ' ');
+//         //console.log(str.trim().split('\t'))
+//         console.log(`Line from file: ${str}`);
+//         zipCodes[str[0]] = {
+//             geoId: parseInt(str[0]),
+//             aLand: parseFloat(str[1]),
+//             aWater: parseFloat(str[2]),
+//             aLandSQMI: parseFloat(str[3]),
+//             aWaterSQMI: parseFloat(str[4]),
+//             lat: parseFloat(str[5]),
+//             lon: parseFloat(str[6])
+//         }
+//     }
+//     console.log(zipCodes);
+//     fs.writeFileSync('./allZipCodes.js', 'module.exports.zipCodes = ' + util.inspect(JSON.stringify(zipCodes)), 'utf-8')
+// }
+
+
+//:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+//:::                                                                         :::
+//:::  This routine calculates the distance between two points (given the     :::
+//:::  latitude/longitude of those points). It is being used to calculate     :::
+//:::  the distance between two locations using GeoDataSource (TM) prodducts  :::
+//:::                                                                         :::
+//:::  Definitions:                                                           :::
+//:::    South latitudes are negative, east longitudes are positive           :::
+//:::                                                                         :::
+//:::  Passed to function:                                                    :::
+//:::    lat1, lon1 = Latitude and Longitude of point 1 (in decimal degrees)  :::
+//:::    lat2, lon2 = Latitude and Longitude of point 2 (in decimal degrees)  :::
+//:::    unit = the unit you desire for results                               :::
+//:::           where: 'M' is statute miles (default)                         :::
+//:::                  'K' is kilometers                                      :::
+//:::                  'N' is nautical miles                                  :::
+//:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+const getDistance = (a, b, unit = 'M') => {
+    let lat1 = a.lat;
+    let lon1 = a.lon;
+    let lat2 = b.lat;
+    let lon2 = b.lon
+
+    if ((lat1 == lat2) && (lon1 == lon2)) {
+        return 0;
+    }
+    else {
+        let radlat1 = Math.PI * lat1 / 180;
+        let radlat2 = Math.PI * lat2 / 180;
+        let theta = lon1 - lon2;
+        let radtheta = Math.PI * theta / 180;
+        let dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+        if (dist > 1) {
+            dist = 1;
+        }
+        dist = Math.acos(dist);
+        dist = dist * 180 / Math.PI;
+        dist = dist * 60 * 1.1515;
+        if (unit == "K") { dist = dist * 1.609344 }
+        if (unit == "N") { dist = dist * 0.8684 }
+        return dist;
+    }
+}
+
+// process initial zipcode data to json object
+//processLineByLine();
+
+
+//get distance between two points
+
+//console.log('distance in miles = ', getDistance(zipCodes[98006], zipCodes[90210]))
+
